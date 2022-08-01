5c42a64aab29772cd50a7cc0080a883fb962fff2
andikscript <andikscript@gmail.com>
2022-08-01 22:05:54 +0700

    add find by username and email

--- src/main/java/com/andikscript/springsociallogin/repository/UserRepository.java
+++ src/main/java/com/andikscript/springsociallogin/repository/UserRepository.java
@@ -5,6 +5,12 @@ import com.andikscript.springsociallogin.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
 }
