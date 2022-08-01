699427da48efab903b7438ce6bbc76d9f271003c
andikscript <andikscript@gmail.com>
2022-08-01 22:33:11 +0700

    add repository user and refresh token

--- src/main/java/com/andikscript/springsociallogin/repository/RefreshRepository.java
+++ src/main/java/com/andikscript/springsociallogin/repository/RefreshRepository.java
@@ -1,6 +1,6 @@
 package com.andikscript.springsociallogin.repository;
 
-
+import com.andikscript.springsociallogin.model.RefreshToken;
 import com.andikscript.springsociallogin.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
@@ -8,9 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String> {
-
-    Optional<User> findByUsername(String username);
-
-    Optional<User> findByEmail(String email);
+public interface RefreshRepository extends MongoRepository<RefreshToken, String> {
+    Optional<RefreshToken> findByToken(String token);
+    void deleteByIdUser(User user);
 }
