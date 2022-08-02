8e0952e797e58af21d3bacce6760de6b780dd83c
andikscript <andikscript@gmail.com>
2022-08-02 17:06:56 +0700

    remove row code useless

--- src/main/java/com/andikscript/springsociallogin/security/SecurityConfiguration.java
+++ src/main/java/com/andikscript/springsociallogin/security/SecurityConfiguration.java
@@ -2,7 +2,6 @@ package com.andikscript.springsociallogin.security;
 
 import com.andikscript.springsociallogin.security.jwt.AuthEntryPointJwt;
 import com.andikscript.springsociallogin.security.jwt.AuthTokenFilter;
-import com.andikscript.springsociallogin.security.service.UserDetailsImpl;
 import com.andikscript.springsociallogin.security.service.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,11 +11,8 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
