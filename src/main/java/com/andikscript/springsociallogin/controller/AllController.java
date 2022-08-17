203bd97d20676a2f942465f4415eaf7b48257646
andikscript <andikscript@gmail.com>
2022-08-17 16:54:09 +0700

    test controller for annotation root

--- src/main/java/com/andikscript/springsociallogin/controller/AllController.java
+++ src/main/java/com/andikscript/springsociallogin/controller/AllController.java
@@ -1,5 +1,6 @@
 package com.andikscript.springsociallogin.controller;
 
+import com.andikscript.springsociallogin.access.IsRoot;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/api/test/all")
 public class AllController {
 
+    @IsRoot
     @GetMapping(value = "/access")
     public String adminAccess() {
         return "all access";
