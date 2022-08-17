0a8592664261e6fa5118d60e8abdba39e25284f1
andikscript <andikscript@gmail.com>
2022-08-17 09:08:46 +0700

    change annotation for limit access

--- src/main/java/com/andikscript/springsociallogin/security/SecurityConfiguration.java
+++ src/main/java/com/andikscript/springsociallogin/security/SecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -19,8 +20,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(
-        prePostEnabled = true,
+@EnableMethodSecurity(
         securedEnabled = true
 )
 public class SecurityConfiguration {
